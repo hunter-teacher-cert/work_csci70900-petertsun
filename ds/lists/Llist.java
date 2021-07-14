3c0fade9f1be5ab4f8499017b3fd32be1be46f69
petertsun <petertsun@gmail.com>
2021-07-14 15:36:22 -0400

    Linked list programs

---
+++ ds/lists/Llist.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Llist{
+  private Node front; // the front of the list
+
+  public  Llist(){
+    front = new Node();
+  }
+
+  // Add a new node containing data
+  // at the front of the list
+  public void addFront(String data){
+    // make the new node
+    Node newNode = new Node(data);
+
+    // point it to what front points to
+    newNode.setNext(front.getNext());
+
+    // point front to the new node
+    front.setNext(newNode);
+
+  }
+
+  public String toString(){
+    Node currentNode;
+    currentNode = front;
+    String result = "";
+    while (currentNode != null){
+      result = result + currentNode + "=>";
+      // this is like i=i+1 is for arrays
+      // but for linked lists
+      currentNode = currentNode.getNext();
+    }
+    result = result + "null";
+    return result;
+
+  }
+
+}
