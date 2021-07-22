383cae5445d8b09fbbf78e036420dd366606b108
petertsun <petertsun@gmail.com>
2021-07-22 08:39:51 -0400

    doubly linked list

---
+++ ds/dlist/Driver.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args) {
+	// Node n;
+	// n = new Node();
+	// n.setData("Eduardo");
+  //
+  //
+	// Node n2 = new Node("Brian");
+	// n.setNext(n2);
+  //
+	// System.out.println(n);
+  //
+	// System.out.println(n2);
+  //
+	// System.out.println(n.getNext());
+	// n.getNext().setNext(new Node("Steph"));
+	// System.out.println(n2.getNext());
+	// System.out.println(n.getNext().getNext());
+
+	//Node L;
+	// 1.Create a new  list that looks like:
+	//   L->"a"->"b"->"c"->"d"
+	// 2. Write the code to insert an "x"
+	//    between the b and the c
+	// 3. Write the code to delete the c
+
+/*
+  Node L = new Node();
+  Node d = new Node("d");
+  Node c = new Node("c", d);
+  Node b = new Node("b", c);
+  Node a = new Node("a", b);
+  L.setNext(a);
+
+  Node tempNode = L;
+  while (tempNode != null) {
+    System.out.print(tempNode.getData() + " ");
+    tempNode = tempNode.getNext();
+  }
+  System.out.println("null");
+
+  System.out.println("Part2");
+
+  Node x = new Node("x", c);
+  b.setNext(x);
+
+  tempNode = L;
+  while (tempNode != null) {
+    System.out.print(tempNode.getData() + " ");
+    tempNode = tempNode.getNext();
+  }
+  System.out.println("null");
+
+
+
+    System.out.println("Part3");
+  x.setNext(d);
+
+  tempNode = L;
+  while (tempNode != null) {
+    System.out.print(tempNode.getData() + " ");
+    tempNode = tempNode.getNext();
+  }
+  System.out.println("null");
+*/
+     Dlist testLinkedList = new Dlist();
+     testLinkedList.addFront("Tommy");
+     testLinkedList.addFront("Sammy");
+     testLinkedList.addFront("Amy");
+     System.out.println(testLinkedList);
+     //System.out.println(testLinkedList.front);
+     //System.out.println(testLinkedList.back);
+     testLinkedList.insert(1, "Denny");
+     testLinkedList.insert(0, "Vinny");
+     System.out.println(testLinkedList);
+     System.out.println(testLinkedList.search("Sammy"));
+
+
+     System.out.println(testLinkedList);
+     testLinkedList.remove(0);
+     System.out.println(testLinkedList);
+     testLinkedList.remove(2);
+     System.out.println(testLinkedList);
+     /*
+     System.out.println(testLinkedList);
+     testLinkedList.remove(1);
+     System.out.println(testLinkedList);
+     testLinkedList.remove(0);
+     System.out.println(testLinkedList);
+     testLinkedList.remove(0);
+     System.out.println(testLinkedList);
+     testLinkedList.remove(0);
+     System.out.println(testLinkedList);
+*/
+    }
+}
---
+++ ds/dlist/Node.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Node {
+
+  private String data;
+  private Node next;
+  private Node prev;
+
+  public Node() {
+    data = "";
+    next = null;
+    prev = null;
+  }//default constructor
+
+  public Node(String value) {
+
+    data = value;
+    next = null;
+    prev = null;
+  }//constructor(vale)
+
+  public Node(String value, Node next, Node prev) {
+    data = value;
+    this.next = next;
+    this.prev = prev;
+  }//constructor(value, next)
+
+  public void setData(String value) {
+    data = value;
+  }//setData
+
+  public void setNext(Node n) {
+    next = n;
+  }//setNext
+
+  public void setPrev(Node n) {
+    prev = n;
+  }//setPrev
+
+  public String getData() {
+    return data;
+  }//end getValue
+
+  public Node getNext() {
+    return next;
+  }//getNext
+
+  public Node getPrev() {
+    return prev;
+  }//getPrev
+
+  public String toString() {
+    return data;
+  }//toString
+
+}//class Node
